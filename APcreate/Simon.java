cf1cf5d72b1c83246cb16fad44b7582c8a28ef3f
Victor Castro II <victor.castro@mrjaffesclass.com>
2023-01-10 11:45:17 -0800

    salfljdsa

--- APcreate/Simon.java
+++ APcreate/Simon.java
@@ -1,3 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Simon {
+    private String red;
+    private String blue;
+    private String green;
+    private String yellow;
+    int color;
+    public Simon() {
+        //oidsafhdsalkjfdsaaaaaaaalkjsdfhlaksdjlfakjdshflkjdsalf
+    }
     
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        boolean ongoing = true;
+        while (ongoing == true) {
+            System.out.print("Press spacebar to start...");
+            try {
+                color= scanner.next();
+                
+            } catch (InputMismatchException error) {
+                
+            }
+        }
+    }
+    
+    public void main(String[]args) {
+        
+    }
 }
