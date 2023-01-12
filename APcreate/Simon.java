ca5c939042e9ec6b6ab02232b93ec9c8de35db4f
Victor Castro II <victor.castro@mrjaffesclass.com>
2023-01-12 11:43:20 -0800

    saldkfa

--- APcreate/Simon.java
+++ APcreate/Simon.java
@@ -6,7 +6,7 @@ public class Simon {
     public Simon() {
         //oidsafhdsalkjfdsaaaaaaaalkjsdfhlaksdjlfakjdshflkjdsalf
     }
-    public void generateRandom() {
+    public String generateRandom() {
         String[]colors;
         colors = new String[4];
         colors[0] = "red";
@@ -14,11 +14,31 @@ public class Simon {
         colors[2] = "green";
         colors[3] = "yellow";
         int random = (int) (Math.random()*4);
-        System.out.println(colors[random]);
         this.color = colors[random];
+        return this.color;
+    }
+    public void generateRandomSequence() {
+        String[]sqnc = new String[16];
+        sqnc[0]= generateRandom();
+        sqnc[1]= generateRandom();
+        sqnc[2]= generateRandom();
+        sqnc[3]= generateRandom();
+        sqnc[4]= generateRandom();
+        sqnc[5]= generateRandom();
+        sqnc[6]= generateRandom();
+        sqnc[7]= generateRandom();
+        sqnc[8]= generateRandom();
+        sqnc[9]= generateRandom();
+        sqnc[10]= generateRandom();
+        sqnc[11]= generateRandom();
+        sqnc[12]= generateRandom();
+        sqnc[13]= generateRandom();
+        sqnc[14]= generateRandom();
+        sqnc[15]= generateRandom();
     }
     public void go() {
         boolean shouldGo = true;
+        Scanner scanner = new Scanner(System.in);
         while (shouldGo == true) {
             
         }
