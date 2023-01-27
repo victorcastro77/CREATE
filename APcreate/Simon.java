ffc3bba7d366bb4fbe01147805a0ec6a77417fd1
Victor Castro II <victor.castro@mrjaffesclass.com>
2023-01-27 11:42:48 -0800

    dlsakf

--- APcreate/Simon.java
+++ APcreate/Simon.java
@@ -2,10 +2,10 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
-
 public class Simon {
     private String color;
-    ArrayList<String> sqnc = new ArrayList<>();
+    String[] sqnc = new String[16];
+    int a;
     public Simon() {
         //oidsafhdsalkjfdsaaaaaaaalkjsdfhlaksdjlfakjdshflkjdsalf
     }
@@ -17,30 +17,37 @@ public class Simon {
         return this.color;
     }
 
-    public void generateRandomSequence() {
-        ArrayList<String> sqnc1 = new ArrayList<>(Arrays.asList(generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom(),generateRandom()));
-        sqnc = sqnc1;
+    public String[] generateRandomSequence() {
+        sqnc[0] = generateRandom();
+        sqnc[1] = generateRandom();
+        sqnc[2] = generateRandom();
+        sqnc[3] = generateRandom();
+        sqnc[4] = generateRandom();
+        sqnc[5] = generateRandom();
+        sqnc[6] = generateRandom();
+        sqnc[7] = generateRandom();
+        sqnc[8] = generateRandom();
+        sqnc[9] = generateRandom();
+        sqnc[10] = generateRandom();
+        sqnc[11] = generateRandom();
+        sqnc[12] = generateRandom();
+        sqnc[13] = generateRandom();
+        sqnc[14] = generateRandom();
+        sqnc[15] = generateRandom();
+        return sqnc;
     }
 
-    public void go() {
+    public void main(String[] args) {
         boolean shouldGo = true;
         Scanner input = new Scanner(System.in);
+        int in = input.nextInt();
         while (shouldGo) {
-            for (int i = 0; i < sqnc.size(); i++) {
-                int a = sqnc.get(i);
-                System.out.print(a + " ");
+            for (int i = 0; i < 16; i++) {
+                a = i;
             }
-            System.out.println("/nMatch the pattern:");
-            for (int i = 0; i < sqnc.size(); i++) {
-                int button = input.nextInt();
-                if (button != a) {
-                    System.out.println("Incorrect! Game over.");
-                    shouldGo = false;
-                    break;
-                }
+            if (in == a) {
+                
             }
         }
-
-
     }
 }
